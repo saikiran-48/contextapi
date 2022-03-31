15adb96b6a5c5a8fd56c4539436a247bd49a7086
saikiran kadaveru <kirannetha97@gmail.com>
2022-04-01 03:06:09 +0530

    provided styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,26 @@
 import React, { useContext } from "react";
-import { LoginContext } from "./Contexts/LoginContex";
+import { LoginContext } from "../Contexts/LoginContex";
+import "./Login.css";
 export default function Login() {
   const { setUsername, setShowProfile } = useContext(LoginContext);
 
   return (
     <>
-      <div>
+      <div className="wrapper">
+        <h1>Context API</h1>
+
         <input
           type="text"
           placeholder="Username"
+          className="input-field"
           onChange={(e) => {
             setUsername(e.target.value);
           }}
         />
-        <input type="text" placeholder="Password" />
-        <button onClick={() => setShowProfile(true)}>Login</button>
-        {/* {showProfile && <h1> {username} </h1>} */}
+        <input type="text" className="input-field" placeholder="Password" />
+        <button className="btn" onClick={() => setShowProfile(true)}>
+          Login
+        </button>
       </div>
     </>
   );
